bd1856538455af4262b29447f838b966c453ed22
focccus <marten.winkler@wtnet.de>
2020-08-15 14:06:43 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Vocabular
 
-A new Flutter project.
+![InApp Screenshots](https://i.imgur.com/EGaqMIA.png)
 
-## Getting Started
+Vocabular is a mobile/web app that helps learning Vocabulary.
+Visit it at: https://focccus.github.io/vocabular/
 
-This project is a starting point for a Flutter application.
+## Features
 
-A few resources to get you started if this is your first Flutter project:
+The app is structured like Vocabulary Boxes. You can create infinitly many boxes that act as a collection of Vocabulary Pairs.
+In the settings you can change the language, how many compartments the box has and more.
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- You can quickly enter Vocabs after each other.
+- Optionally you can enable Declensions for each box and learn them with the translation.
+- The app will suggest Vocabs you already entered and use MyMemory translations as suggestions.
+- With the Trainer you can learn your vocabs. Words go through multiple stages depending on success. You can learn the weakest performing the most.
+- You can decide if the word was correct or not or input it with your keyboard and let the app check it automatically.
+- The App uses String similarity, so slight spelling mistakes, uppercase and hyphens are ignored. The goal is to learn the words not the exact input.
+- Once finished with learning, you can test your knowledge. The App will evaluate the answers, calculate mistakes and gives you statistics on your grade.
+- The app supports multiple grade types: American, German, Points.
+- You can fully customize the color scheme.
+- The app has dark mode
+- Add new languages to learn.
+- You can browse all your words in all boxes and search for specific words.
+- Share your Boxes by exporting and importing `.vocab` files. Your friends can use the same words that you learn!
+- The app uses no external server, service or ads. Everything you do is kept on device.
+- Responsive. Use the app on all screen sizes and devices.
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+This app was made with Material Design in Flutter. It uses HiveDB to store vocabs locally.
--- vocabs/download.dart
+++ vocabs/download.dart
@@ -3,24 +3,34 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 
-const subjectID = '2cf779f8-ef18-4968-9032-321e410ffd58';
+const subjects = {
+  'Green Line 2': 'eb7b6da8-f9c0-4529-8a59-99d91c54d3c4',
+  'Green Line 5': '36d63a43-2e40-4185-850f-c05f3cdb5edd',
+  'Green Line 4': '6dd4513a-4839-488c-99c9-b2e701363612',
+  'Green Line 3': 'c17fa59d-b6b2-4671-bfbe-307054616404',
+};
 
-const getUnits = "https://lernen.phase-6.de/server.integration/unitsFiltered";
-const getCards = "https://lernen.phase-6.de/server.integration/cardList";
+const lang = 'en';
 
 final dio = Dio(
   BaseOptions(
     baseUrl: 'https://lernen.phase-6.de/server.integration/',
     headers: {
       'x-jauthtoken':
-          'walav45463@mailmyrss.com_51c182e7-163d-4612-842b-26b52924217b',
-      'x-clientid': '5cc31107-c14b-413a-f17a-60b50dcf29de',
+          'walav45463@mailmyrss.com_4ade4b13-60a6-4ff1-8566-c964e689033e',
+      'x-clientid': '84a62081-7982-4c93-ef69-e704b5ac5961',
       'x-lbtoken': 'walav45463@mailmyrss.com',
     },
   ),
 );
 
-void main(List<String> args) async {
+main(List<String> args) {
+  subjects.forEach((key, value) {
+    getSubject(value, './$key/');
+  });
+}
+
+void getSubject(String subjectID, String dir) async {
   Map<String, String> units = {};
 
   final unitRes = (await dio.post(
@@ -48,43 +58,71 @@ void main(List<String> args) async {
   } else
     return;
 
-  getUnit(units.keys.first, units.values.first);
+  void getUnit(String id, String name) async {
+    final res = (await dio.post(
+      'cardList',
+      data: {
+        'subjectId': subjectID,
+        'units': [id],
+      },
+    ))
+        .data;
+
+    if (res != null &&
+        res['replyContent'] != null &&
+        res['replyContent']['cards'] != null) {
+      List<Map<String, String>> vocabs = [];
+
+      for (var unit in res['replyContent']['cards']) {
+        if (unit['cardContent'] != null &&
+            unit['cardContent']['question'] != null &&
+            unit['cardContent']['answer'] != null) {
+          String main = unit['cardContent']['question'];
+          String other = unit['cardContent']['answer'];
+
+          other = stringUnify(other);
+
+          main = stringUnify(main);
+
+          vocabs.add({'main': main, 'other': other});
+        }
+      }
+      final map = {
+        'vocabs': vocabs,
+        'box': {
+          'title': name,
+          'lang': lang,
+          'hasFormen': false,
+          'mustType': false,
+          'goback': 1,
+          'random': true,
+          'askForeign': false,
+          'testCount': -1,
+        }
+      };
+
+      if (Directory(dir).existsSync() == false && dir.isNotEmpty) {
+        Directory(dir).createSync(recursive: true);
+      }
+
+      final file =
+          File(dir + name.replaceAll('/', '').replaceAll('.', '') + '.vocab');
+      print('saved $name');
+
+      file.writeAsString(json.encode(map));
+    } else
+      return;
+  }
+
+  units.forEach(getUnit);
+
+  //getUnit(units.keys.first, units.values.first);
 }
 
-void getUnit(String id, String name) async {
-  final res = (await dio.post(
-    'cardList',
-    data: {
-      'subjectId': subjectID,
-      'units': [id],
-    },
-  ))
-      .data;
-
-  if (res != null &&
-      res['replyContent'] != null &&
-      res['replyContent']['cards'] != null) {
-    List<Map<String, String>> vocabs = [];
-
-    for (var unit in res['replyContent']['cards']) {
-      if (unit['cardContent'] != null &&
-          unit['cardContent']['question'] != null &&
-          unit['cardContent']['answer'] != null) {
-        String main = unit['cardContent']['question'];
-        String other = unit['cardContent']['answer'];
-        other = other
-            .replaceAll('<b>', '')
-            .split('</b>')
-            .first
-            .replaceAll(',', '|');
-        main = main.replaceAll(',', '|');
-
-        vocabs.add({'main': main, 'other': other});
-      }
-    }
-
-    final file = File('./' + name + '.vocab');
-    file.writeAsString(json.encode({'vocabs': vocabs}));
-  } else
-    return;
+String stringUnify(String s) {
+  return s
+      .split('\n')
+      .first
+      .replaceAll(RegExp(r'<[^>]*>'), "")
+      .replaceAll(',', ' |');
 }
